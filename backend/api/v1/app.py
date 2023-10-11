f2c6ce23592b0a92b023df9b9284449caa07a1ba
Maxwell D. Dorliea <deboy170@gmail.com>
2023-10-11 00:09:54 +0000

    Added CORS middleware

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.v1.routers.student import student_router
 from api.v1.routers.note import note_router
 from api.v1.routers.s_class import class_router
@@ -9,6 +10,21 @@ app = FastAPI(
         title="Synelia Full Stack Job Assessment API",
         docs_url="/"
         )
+
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 
 app.include_router(student_router)
