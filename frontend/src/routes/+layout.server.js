1a7717245ab42b33c37c000a21c935e162f30dc4
Maxwell D. Dorliea <deboy170@gmail.com>
2023-10-12 09:43:43 +0000

    Fixed a bug of the home route not loading anything

--- frontend/src/routes/+layout.server.js
+++ frontend/src/routes/+layout.server.js
@@ -1,5 +1,10 @@
+import { redirect } from "@sveltejs/kit";
+
 export const load = async ({ url }) => {
 	const tab = await url.searchParams.get("tab");
+	if (url.pathname === "/" && tab === null) {
+	  throw redirect(301, "/?tab=Student");
+	}
 	return {
 		tab,
 	}
